f08f79a1bb6ba38c0e8baa23abca356c0b65bc9d
Darko Danchev <dancevdarko@gmail.com>
2018-12-04 12:35:48 +0200

    Add init project

---
+++ src/main/java/com/fmi/department/student/education/DepartmentOfStudentEducation/DepartmentOfStudentEducationApplication.java
@@ -0,0 +1,12 @@
+package com.fmi.department.student.education.DepartmentOfStudentEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentOfStudentEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentOfStudentEducationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fmi/department/student/education/DepartmentOfStudentEducation/DepartmentOfStudentEducationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fmi.department.student.education.DepartmentOfStudentEducation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DepartmentOfStudentEducationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

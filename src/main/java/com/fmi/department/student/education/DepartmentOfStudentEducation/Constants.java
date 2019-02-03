a2b9b5166ef909ac02ca7bb28987eb3349118683
Darko Danchev <dancevdarko@gmail.com>
2019-02-03 14:40:05 +0200

    Add user model

---
+++ src/main/java/com/fmi/department/student/education/DepartmentOfStudentEducation/Constants.java
@@ -0,0 +1,11 @@
+package com.fmi.department.student.education.DepartmentOfStudentEducation;
+
+public class Constants {
+
+    private Constants() {}
+
+    public  static final String  email_regex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+
+    public static final String phone_regex = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$";
+
+}
---
+++ src/main/java/com/fmi/department/student/education/DepartmentOfStudentEducation/model/User.java
@@ -0,0 +1,47 @@
+package com.fmi.department.student.education.DepartmentOfStudentEducation.model;
+
+
+import com.fmi.department.student.education.DepartmentOfStudentEducation.Constants;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false)
+public class User extends Model {
+
+    @Column(name = "username")
+    @NotNull
+    @Length(min = 4, max = 64)
+    private String username;
+
+    @Column(name = "password")
+    @NotNull
+    @Length(min = 8,max = 128)
+    private String password;
+
+    @Column(name = "email")
+    @NotNull
+    @Length(min = 5,max = 64)
+    @Pattern(regexp = Constants.email_regex)
+    private String email;
+
+    @Column(name = "pin")
+    @NotNull
+    private Long pin;
+
+    @Column(name = "phoneNumber")
+    @NotNull
+    @Pattern(regexp = Constants.phone_regex)
+    private String phoneNumber;
+
+}
